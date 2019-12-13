799b1e415cdaa6f7939a4fd904e9867e192c7c5c
Atia Amin <52665195+atiaamin2019@users.noreply.github.com>
2019-12-13 14:45:23 -0500

    Updating bac_lib.py
    from collections import defaultdict
    
    bacteria = defaultdict(int)
    
    
    infile = '/Users/aamin/Desktop/Library4.csv'
    outfile= '/Users/aamin/Desktop/Library4_bac.csv'
    
    total_count = 0
    with open(infile, 'r') as file:
    	for line in file:
    		if line.startswith("Bacteria"):
    			line = str(line.rstrip('\n'))
    			bacteria[line] +=1
    			total_count +=1
    					
    
    my_list = bacteria.keys()
    
    
    print('Total Bacterial reads count:' + str(total_count))
    
    
    with open(outfile, 'w') as fout:
    	for seq, count in bacteria.items():
    		fout.write("{}\t{}\n".format(seq, count))

--- bac_lib.py
+++ bac_lib.py
@@ -1,18 +1,38 @@
+from collections import defaultdict
+
+bacteria = defaultdict(int)
+
+
 infile = '/Users/aamin/Desktop/Library1.csv'
 outfile= '/Users/aamin/Desktop/Library1_bac.csv'
 
-my_list = []
+
 total_count = 0
 
 with open(infile, 'r') as file:
 	for line in file:
-		line = str(line.rstrip('\n'))
 		if line.startswith("Bacteria") and line not in my_list:
+			line = str(line.rstrip('\n'))
+			bacteria[line] +=1
 			total_count += 1
-			my_list.append(line)
+			
+my_list = bacteria.keys()
+
+
+print('Total Bacterial reads count:' + str(total_count))
+
+
+with open(outfile, 'w') as fout:
+	for seq, count in bacteria.items():
+		fout.write("{}\t{}\n".format(seq, count))
+
 
 			
-print('Total Bacterial Read Count:', total_count)
-print(my_list)
-with open(outfile, "w") as output:
-	output.write(str(my_list))
+
+
+
+
+
+
+					
+
